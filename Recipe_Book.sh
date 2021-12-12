cb2d4d9200f8ea86913bda0ed7e8d1b65aa3bf02
Ascetic313 <iddrisualmuqadas74@gmail.com>
2021-12-12 22:52:36 +0000

    This is a project on recipe book

---
+++ Recipe_Book.sh
@@ -0,0 +1,18 @@
+$ # Checking local remote:
+$ cd veggie-favorites
+$ git add chili.txt
+$ git add squash-lasagna.txt
+
+# Changing Directory:
+$ cd ../veggie-clone
+
+# Changing a Branch:
+$ git branch new-recipe.txt
+
+# Committing new changes:
+$ git commit -m "Thisare few changes"
+
+
+# Switching back to Directory:
+$ cd ../veggie-favorites
+$ 
